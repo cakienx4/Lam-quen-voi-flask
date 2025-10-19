54583a7bcc579811bcfbebeb475458fccef1ecb4
cakienx4 <cakien197@gmail.com>
2025-10-19 20:22:33 +0700

    Viết thêm trang hiển thị thông tin người dùng vừa nhập

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('web.html')
+def main():
+    return render_template('main.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    ten = request.form.get('ten')
+    tuoi = request.form.get('tuoi')
+    que = request.form.get('que')
+    congviec = request.form.get('congviec')
+    diem = request.form.get('diem')
+    return render_template('thongtin.html',
+                           ten=ten,
+                           tuoi=tuoi,
+                           que=que,
+                           congviec=congviec,
+                           diem=diem)        
 
 if __name__ == '__main__':
     app.run(debug=True)
